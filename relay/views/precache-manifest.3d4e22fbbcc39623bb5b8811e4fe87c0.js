141e89ba7f155ecf4689388795842629631ef80a
mcmoe <mcxmoe@gmail.com>
2022-08-19 16:17:42 +0200

    Update outdated (and broken) relay server release/views to match head client/build

---
+++ relay/views/precache-manifest.3d4e22fbbcc39623bb5b8811e4fe87c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9d9fa3c6e1fc68055cb813d9462014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533f025c514b70a5df98",
+    "url": "/static/css/2.66519fc2.chunk.css"
+  },
+  {
+    "revision": "533f025c514b70a5df98",
+    "url": "/static/js/2.9aa2c951.chunk.js"
+  },
+  {
+    "revision": "b606720f33c3ad4a22e6",
+    "url": "/static/js/main.e8da414a.chunk.js"
+  },
+  {
+    "revision": "1a2f2821d175e0fa084d",
+    "url": "/static/js/runtime-main.9329b01b.js"
+  }
+]);
--- relay/views/service-worker.js
+++ relay/views/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.292109c6d4af4e528c81b54af02ca5c4.js"
+  "/precache-manifest.3d4e22fbbcc39623bb5b8811e4fe87c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
